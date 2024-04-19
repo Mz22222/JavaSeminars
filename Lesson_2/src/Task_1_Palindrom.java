9dc01aa952b2da6ac81e2c4c11f0358415a11a6c
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-04-19 21:08:59 +0300

    add Lesson_2

---
+++ Lesson_2/src/Task_1_Palindrom.java
@@ -0,0 +1,26 @@
+//Task_1
+//Напишите метод, который принимает на вход строку (String) и
+//определяет является ли строка палиндромом (возвращает boolean значение).
+
+public class Task_1_Palindrom {
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(" анна "));
+    }
+
+    public static boolean isPalindrom(String str) {
+        str = str.replace(" ", "").trim().toLowerCase();
+        char[] charArray = str.toCharArray();
+        for (int i = 0; i < charArray.length/2; i++) {
+            int length = charArray.length;
+            if (str.charAt(i) != str.charAt(length-i-1)) {
+                System.out.println("Это не палиндром");
+
+                return false;
+            }
+
+        }
+        System.out.println("Это палиндром");
+        return true;
+
+    }
+    }
