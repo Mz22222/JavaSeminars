da1903b4c3ffefdfd64b1ec1799d696943318550
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-04-28 00:36:32 +0300

    add Lesson_3:Task0

---
+++ Lesson_3/src/Task0.java
@@ -0,0 +1,14 @@
+//Task0
+//Даны следующие строки, сравнить их с помощью == и метода equals0 класса Object
+//String s1 = "hello";
+//String s2 = "hello";
+//String s3 = s1; 1
+//String s4 = "h" + "e" + "I" + "|" + "0";
+//String s5 = new String("hello");
+//String s6 = new String(new charl'h', 'e', t', Y', 'o'});
+
+public class Task0 {
+    public static void main(String[] args) {
+
+    }
+}
